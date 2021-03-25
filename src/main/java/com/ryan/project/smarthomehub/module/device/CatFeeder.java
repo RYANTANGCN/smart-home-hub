b5d33db3feec2a25c873331aa986517b27d97c8e
TangQianli <tangqianli@pin-dao.cn>
2021-03-25 17:59:20 +0800

    fix: fixe dispense command error

--- src/main/java/com/ryan/project/smarthomehub/module/device/CatFeeder.java
+++ src/main/java/com/ryan/project/smarthomehub/module/device/CatFeeder.java
@@ -3,6 +3,7 @@ package com.ryan.project.smarthomehub.module.device;
 import cn.hutool.core.map.MapUtil;
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.cloud.firestore.FieldValue;
 import com.ryan.project.smarthomehub.config.DeviceType;
 import com.ryan.project.smarthomehub.module.trait.Dispense;
 import lombok.extern.slf4j.Slf4j;
@@ -32,8 +33,16 @@ public class CatFeeder extends Device implements Dispense {
         String item = (String) params.get("item");
 
         try {
-            Integer amountRemaining = (Integer) documentReference.get().get().get("state.amountRemaining.amount");
-            documentReference.update("state.amountRemaining.amount", amountRemaining - amount);
+            List<Map<String, Object>> states = (List<Map<String, Object>>) documentReference.get().get().get("states");
+            Map<String, Object> state = states.stream().filter(map -> params.get("item").equals(map.get("itemName"))).findFirst().get();
+            documentReference.update("states", FieldValue.arrayRemove(state));
+            Map<String, Object> amountLastDispensed = (Map<String, Object>) state.get("amountLastDispensed");
+            amountLastDispensed.put("amount", params.get("amount"));
+
+            Map<String, Object> amountRemaining = (Map<String, Object>) state.get("amountRemaining");
+            amountRemaining.put("amount", (Long) amountRemaining.get("amount") - (Integer) params.get("amount"));
+
+            documentReference.update("states", FieldValue.arrayUnion(state));
         } catch (InterruptedException e) {
             e.printStackTrace();
         } catch (ExecutionException e) {
