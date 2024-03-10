966a683c66f1c54e264479b7901db2a7894e51b6
ryantangcn <ryantangcn@gmail.com>
2024-03-10 19:08:25 +0800

    fixed: heater device

--- src/main/java/com/ryan/project/smarthomehub/module/device/Heater.java
+++ src/main/java/com/ryan/project/smarthomehub/module/device/Heater.java
@@ -2,13 +2,17 @@ package com.ryan.project.smarthomehub.module.device;
 
 import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.DocumentSnapshot;
+import com.ryan.project.smarthomehub.config.DeviceType;
 import com.ryan.project.smarthomehub.module.trait.TemperatureSetting;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
 import java.util.Map;
 
 @Slf4j
-@Component
+@DeviceType("action.devices.types.HEATER")
+@Service("smart_heater")
 public class Heater extends Device implements TemperatureSetting {
     @Override
     public Map<String, Object> processQuery(DocumentSnapshot device, Map<String, Object> customData) {
