8c8e5f54f581b6206249e9ed98b9047a2d191f24
ryantangcn <ryantangcn@gmail.com>
2024-03-11 14:53:08 +0800

    fixed: heater commands

--- src/main/java/com/ryan/project/smarthomehub/module/device/Heater.java
+++ src/main/java/com/ryan/project/smarthomehub/module/device/Heater.java
@@ -1,14 +1,17 @@
 package com.ryan.project.smarthomehub.module.device;
 
 import com.google.cloud.firestore.DocumentReference;
-import com.google.cloud.firestore.DocumentSnapshot;
+import com.google.gson.Gson;
 import com.ryan.project.smarthomehub.config.DeviceType;
 import com.ryan.project.smarthomehub.module.trait.OnOff;
 import com.ryan.project.smarthomehub.module.trait.TemperatureSetting;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Component;
+import org.eclipse.paho.client.mqttv3.MqttAsyncClient;
+import org.eclipse.paho.client.mqttv3.MqttMessage;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -16,13 +19,52 @@ import java.util.Map;
 @Service("smart_heater")
 public class Heater extends Device implements TemperatureSetting, OnOff {
 
+    @Autowired
+    MqttAsyncClient mqttAsyncClient;
+
+    private Gson gson = new Gson();
+
     @Override
-    public void processTemperatureSetPoint(DocumentReference documentSnapshot, Map<String, Object> params) {
+    public void processTemperatureSetPoint(DocumentReference documentReference, Map<String, Object> params) {
         log.info("received command:{}", params);
+        Integer degrees = (Integer) params.get("thermostatTemperatureSetpoint");
+        try {
+            //mqtt
+            Map<String, Object> mqttMsg = new HashMap<>();
+            mqttMsg.put("setpoint", degrees);
+            MqttMessage mqttMessage = new MqttMessage(gson.toJson(mqttMsg).toString().getBytes());
+            mqttMessage.setQos(0);
+
+            //send command to mqtt
+            String topic = "/heater/" + documentReference.getId();
+            mqttAsyncClient.publish(topic, mqttMessage);
+
+            //update firestore date
+            documentReference.update("states.thermostatTemperatureSetpoint", degrees);
+        } catch (Exception e) {
+            log.error("", e);
+        }
     }
 
     @Override
     public void processOnOff(DocumentReference documentReference, Map<String, Object> params) {
         log.info("received command:{}", params);
+        boolean on = (Boolean) params.get("on");
+        try {
+            //mqtt
+            Map<String, Object> mqttMsg = new HashMap<>();
+            mqttMsg.put("on", on);
+            MqttMessage mqttMessage = new MqttMessage(gson.toJson(mqttMsg).toString().getBytes());
+            mqttMessage.setQos(0);
+
+            //send command to mqtt
+            String topic = "heater/" + documentReference.getId();
+            mqttAsyncClient.publish(topic, mqttMessage);
+
+            //update firestore date
+            documentReference.update("states.on", on);
+        } catch (Exception e) {
+            log.error("", e);
+        }
     }
 }
