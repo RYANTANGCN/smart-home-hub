5b77caeea341ab82443985c270128d28e4458a1d
ryantangcn <ryantangcn@gmail.com>
2024-03-18 16:31:36 +0800

    build: add auto deploy in github action

--- src/main/java/com/ryan/project/smarthomehub/config/FirestoreConfig.java
+++ src/main/java/com/ryan/project/smarthomehub/config/FirestoreConfig.java
@@ -1,5 +1,9 @@
 package com.ryan.project.smarthomehub.config;
 
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.json.gson.GsonFactory;
+import com.google.api.services.homegraph.v1.HomeGraphService;
+import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.firestore.Firestore;
 import com.google.firebase.FirebaseApp;
@@ -12,6 +16,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.List;
 
 /**
  * @Descritption Firebase configuration class, more detail refer to: https://firebase.google.com/docs/admin/setup
@@ -25,12 +32,20 @@ public class FirestoreConfig {
     @Autowired
     HubProperties hubProperties;
 
+    private GoogleCredentials googleCredentials;
+
+    public GoogleCredentials googleCredentials() throws IOException {
+        if (this.googleCredentials == null) {
+            this.googleCredentials = GoogleCredentials.getApplicationDefault().createScoped();
+        }
+        return this.googleCredentials;
+    }
+
     @Bean
     public Firestore init(){
         try {
-            GoogleCredentials credentials = GoogleCredentials.getApplicationDefault();
             FirebaseOptions options =
-                    new FirebaseOptions.Builder().setCredentials(credentials).setProjectId(hubProperties.getProjectId()).build();
+                    new FirebaseOptions.Builder().setCredentials(googleCredentials()).setProjectId(hubProperties.getProjectId()).build();
             FirebaseApp firebaseApp = FirebaseApp.initializeApp(options);
             Firestore database = FirestoreClient.getFirestore(firebaseApp);
             log.info("Firestore init finished......");
@@ -39,5 +54,20 @@ public class FirestoreConfig {
             log.error("ERROR: invalid service account credentials. See README.");
             throw new RuntimeException(e.getMessage());
         }
+    }
+
+    @Bean
+    public HomeGraphService homeGraphService() throws IOException, GeneralSecurityException {
+
+        // Create Home Graph service client.
+        HomeGraphService homegraphService =
+                new HomeGraphService.Builder(
+                        GoogleNetHttpTransport.newTrustedTransport(),
+                        GsonFactory.getDefaultInstance(),
+                        new HttpCredentialsAdapter(googleCredentials()))
+                        .setApplicationName("Smart-Home-Hub/1.0")
+                        .build();
+
+        return homegraphService;
     }
 }
--- src/test/java/com/ryan/project/smarthomehub/SmartHomeHubApplicationTests.java
+++ src/test/java/com/ryan/project/smarthomehub/SmartHomeHubApplicationTests.java
@@ -4,9 +4,11 @@ import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.json.gson.GsonFactory;
 import com.google.api.services.homegraph.v1.HomeGraphService;
 import com.google.api.services.homegraph.v1.model.RequestSyncDevicesRequest;
+import com.google.api.services.homegraph.v1.model.RequestSyncDevicesResponse;
 import com.google.auth.http.HttpCredentialsAdapter;
 import com.google.auth.oauth2.GoogleCredentials;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
@@ -14,31 +16,21 @@ import java.util.List;
 @SpringBootTest
 class SmartHomeHubApplicationTests {
 
+    @Autowired
+    HomeGraphService homeGraphService;
+
     @Test
     void contextLoads() {
     }
 
     @Test
     void requestSyncTest() throws Exception {
-        // Get Application Default credentials.
-        GoogleCredentials credentials =
-                GoogleCredentials.getApplicationDefault()
-                        .createScoped(List.of("https://www.googleapis.com/auth/homegraph"));
-
-        // Create Home Graph service client.
-        HomeGraphService homegraphService =
-                new HomeGraphService.Builder(
-                        GoogleNetHttpTransport.newTrustedTransport(),
-                        GsonFactory.getDefaultInstance(),
-                        new HttpCredentialsAdapter(credentials))
-                        .setApplicationName("Smart-Home-Hub/1.0")
-                        .build();
 
         // Request sync.
         RequestSyncDevicesRequest request =
                 new RequestSyncDevicesRequest().setAgentUserId("PLACEHOLDER-USER-ID").setAsync(false);
-        homegraphService.devices().requestSync(request);
-
+        RequestSyncDevicesResponse response = homeGraphService.devices().requestSync(request).execute();
+        System.out.println(response.toPrettyString());
     }
 
 }
